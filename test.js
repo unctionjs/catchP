53c0f5914e240f370fa5745b5733cae528a7549c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import catchP from "./source.js"
+import cascadingKeyChain from "./source"
 
 test(({same}) => {
   const rejection = (value) => same(value, new Error("a"))
