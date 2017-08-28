7cf5e938cb69d97a76116cd52aa4ca07ab4edf88
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-28 00:50:50 -0700

    Now nyc will use the non-transpiled version

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import catchP from "./"
+import catchP from "./index"
 
 test(({same}) => {
   const rejection = (value) => same(value, new Error("a"))
